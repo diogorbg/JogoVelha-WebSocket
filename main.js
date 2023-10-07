922fbb1430abbe7620484903cd50fe18414002f3
Diogo RBG <diogorbg@gmail.com>
2023-10-06 22:14:01 -0300

    Update main.js

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ wss.on("connection", function (ws, req) {
       console.log('keepAlive');
       return;
     }
-    broadcast(ws, stringifiedData, false);
+    broadcast(ws, stringifiedData, true);
   });
 
   ws.on("close", (data) => {
@@ -74,7 +74,7 @@ const broadcast = (ws, message, includeSelf) => {
         client.send('ping');
       }
     });
-  }, 50000);
+  }, 5000);
 };
 
 
